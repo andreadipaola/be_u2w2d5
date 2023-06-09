a684660bae25b748fe96a69f2868ab0708e20c15
Andrea Di Paola <andr3a.dipaola@gmail.com>
2023-06-09 11:40:08 +0200

    Implementate relazioni tra entities e test positivo su DB

--- src/main/java/app/dispositivo/Dispositivo.java
+++ src/main/java/app/dispositivo/Dispositivo.java
@@ -3,17 +3,32 @@ package app.dispositivo;
 import java.util.UUID;
 
 import app.dispositivo.enums.StatoDispositivo;
+import app.utente.Utente;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "dispositivi")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Dispositivo {
+	@Id
+	@GeneratedValue
+	@Column(name = "id_dispositivo")
 	private UUID idDispositivo;
 	private String tipo;
+	@Column(name = "stato_dispositivo")
 	private StatoDispositivo statoDispositivo;
+	@ManyToOne
+	@JoinColumn(name = "id_utente")
+	private Utente utente;
 }
--- src/main/java/app/utente/Utente.java
+++ src/main/java/app/utente/Utente.java
@@ -4,19 +4,29 @@ import java.util.Set;
 import java.util.UUID;
 
 import app.dispositivo.Dispositivo;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "utenti")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Utente {
+	@Id
+	@GeneratedValue
+	@Column(name = "id_utente")
 	private UUID idUtente;
 	private String username;
 	private String nome;
 	private String cognome;
+	@OneToMany(mappedBy = "utente")
 	private Set<Dispositivo> dispositivi;
 }
